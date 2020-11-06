492944bcd3f7a0b287f12bb4d442e0e0a032f520
C5313158 <romans.jefremovs@sap.com>
2020-11-06 10:05:26 +0200

    Update for swagger

--- src/main/java/com/bookstore/BookstoreApp.java
+++ src/main/java/com/bookstore/BookstoreApp.java
@@ -2,11 +2,13 @@ package com.bookstore;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import springfox.documentation.swagger2.annotations.EnableSwagger2;
 
 /**
  * Bookstore App.
  */
 @SpringBootApplication
+@EnableSwagger2
 public class BookstoreApp {
     public static void main(String[] args) {
         SpringApplication.run(BookstoreApp.class, args);
